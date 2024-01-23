e5f08ed25f11e65dd4bc459792e72e59134fb02b
Coppermine-SP <son5121502@gmail.com>
2024-01-24 02:19:59 +0900

    feat: load configuration from file

---
+++ Configuration.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace HomNetBridge
+{
+    public static class Configuration
+    {
+        public static HomNetBridgeConfig? Config { get; private set; }
+        public static void LoadConfig(string path)
+        {
+            Logging.Print($"Load configurations from {path}...");
+
+            try
+            {
+                var config = new ConfigurationBuilder()
+                    .AddJsonFile(path)
+                    .Build();
+
+                Config = config.GetSection("HomNetBridgeConfig").Get<HomNetBridgeConfig>();
+            }
+            catch(Exception e)
+            {
+                Logging.Print("Could not load configuration file: " + e.ToString(), Logging.LogType.Error);
+                throw new Exception();
+            }
+        }
+    }
+
+    public class HomNetBridgeConfig
+    {
+        public string CaptureInterfaceName;
+        public string CaptureFilter;
+        public int ReadTimeout;
+
+        public bool ShowVerbose;
+        public bool ShowRaw;
+
+        public string HAUrl;
+        public string HAKey;
+
+        public int RefFloor;
+        public int NotifyUnit;
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("CloudInteractive HomNetBridge - 0.2.0");
+            Console.WriteLine("Copyright (C) 2024 CloudInteractive Inc.\n\n");
+
+            Logging.Print("Initialization Start...");
+
+            try
+            {
+                Configuration.LoadConfig("config.json");
+            }
+            catch
+            {
+                Logging.Print("Initialization Failed!", Logging.LogType.Error);
+            }
         }
     }
 }
