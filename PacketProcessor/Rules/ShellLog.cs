e4f080f19511822cacfbee16dde2615898fff7c5
Coppermine-SP <son5121502@gmail.com>
2024-01-24 02:20:53 +0900

    feat: ShellLog Rule

---
+++ PacketProcessor/Rules/ShellLog.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using PacketDotNet;
+
+namespace HomNetBridge.PacketProcessor
+{
+    public static partial class Rules
+    {
+
+        [Protocol(ProtocolType.Udp)]
+        [StartsWith("[HBM]")]
+        public static void ShellLog(IPPacket packet)
+        {
+            Logging.Print(packet.PayloadToString() ?? "NULL", Logging.LogType.Debug);
+        }
+    }
+}
