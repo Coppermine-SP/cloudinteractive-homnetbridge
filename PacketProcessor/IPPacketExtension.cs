7bd1e94e6f2014f18ea3fc2007990eddb84b2439
Coppermine-SP <son5121502@gmail.com>
2024-01-23 21:03:27 +0900

    feat: IPPacket class extension method: payload data to UTF-8 string

---
+++ PacketProcessor/IPPacketExtension.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+using PacketDotNet;
+using SharpPcap;
+
+namespace HomNetBridge.PacketProcessor
+{
+    public static class IPPacketExtension
+    {
+        public static string? PayloadToString(this IPPacket packet)
+        {
+            Packet extractedPacket;
+            if (packet.Protocol == ProtocolType.Tcp) 
+                extractedPacket = packet.Extract<TcpPacket>();
+            else if (packet.Protocol == ProtocolType.Udp)
+                extractedPacket = packet.Extract<UdpPacket>();
+            else return null;
+
+            var payload = extractedPacket.PayloadData;
+            if (payload is not null)
+            {
+                return Encoding.UTF8.GetString(payload).Trim();
+            }
+            return null;
+        }
+    }
+}
