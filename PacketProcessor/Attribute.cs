cc5ee54f706571d58a7884092181284c7579c3bf
Coppermine-SP <son5121502@gmail.com>
2024-01-23 16:29:07 +0900

    Add project files.

---
+++ PacketProcessor/Attribute.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomNetBridge.PacketProcessor
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    public class PacketProtocolAttribute : Attribute
+    {
+        public string Value { get; }
+
+        public PacketProtocolAttribute(string value)
+        {
+            Value = value;
+        }
+    }
+
+
+    [AttributeUsage(AttributeTargets.Method)]
+    public class StartsWithAttribute : Attribute
+    {
+        public string Value { get; }
+
+        public StartsWithAttribute(string value)
+        {
+            Value = value;
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Method)]
+    public class ContainsAttribute : Attribute
+    {
+        public string Value { get; }
+
+        public ContainsAttribute(string value)
+        {
+            Value = value;
+        }
+    }
+
+
+}
---
+++ PacketProcessor/Rules/ElevatorCalled.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomNetBridge.PacketProcessor
+{
+    public static partial class Rules
+    {
+        [StartsWith("")]
+        public static void ElevatorCalled()
+        {
+
+        }
+    }
+}
