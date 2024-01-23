ea22f64e1f4ddacd48db225d88bdf36d568a07c1
Coppermine-SP <son5121502@gmail.com>
2024-01-24 02:20:12 +0900

    feat: 로그에 method, class 표시

---
+++ Logging.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace HomNetBridge
+{
+    internal static class Logging
+    {
+        public static bool UseVerbose = false;
+        public static bool UseDebug = false;
+
+        public enum LogType { Raw = 0, Debug = 1, Info = 2, Warn = 3, Error = 4 }
+
+        private static readonly (ConsoleColor, string)[] prefixes =
+        {
+            (ConsoleColor.DarkMagenta, "[RAW]"),
+            (ConsoleColor.Cyan, "[i]"),
+            (ConsoleColor.Blue, "[i]"),
+            (ConsoleColor.DarkYellow, "[!]"),
+            (ConsoleColor.DarkRed, "[!]")
+        };
+
+        public static void Print(string message, LogType type = LogType.Info)
+        {
+            if (type == LogType.Debug && !UseDebug) return;
+            if (type == LogType.Raw && !UseVerbose) return;
+
+            var prefix = prefixes[(int)type];
+
+            Console.ForegroundColor = prefix.Item1;
+            Console.Write(prefix.Item2);
+
+            if (type == LogType.Raw)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+            }
+            else
+            {
+                var stackTrace = new StackTrace();
+                var frame = stackTrace.GetFrame(1);
+                var method = frame?.GetMethod()?.Name ?? "NULL";
+                var className = frame?.GetMethod()?.DeclaringType?.Name ?? "NULL";
+
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+                Console.Write($" [{className}.{method}]: ");
+                Console.ForegroundColor = ConsoleColor.Gray;
+            }
+
+            Console.WriteLine(message);
+        }
+
+    }
+}
