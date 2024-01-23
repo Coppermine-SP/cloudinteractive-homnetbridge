4ba913c82b0904566100b3634126c696e2cc6bbd
Coppermine-SP <son5121502@gmail.com>
2024-01-24 04:30:00 +0900

    feat: 로그 출력시 정규식 적용하여 공백 문자 제거를 통한 가독성 증대

--- PacketProcessor/Rules/ShellLog.cs
+++ PacketProcessor/Rules/ShellLog.cs
@@ -1,16 +1,18 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 using PacketDotNet;
 
 namespace HomNetBridge.PacketProcessor
 {
     public static partial class Rules
     {
-
         [Protocol(ProtocolType.Udp)]
         [StartsWith("[HBM]")]
         public static void ShellLog(IPPacket packet)
         {
-            Logging.Print(packet.PayloadToString() ?? "NULL", Logging.LogType.Debug);
+            const string pattern = @"[\p{C}]|\t|[ ]{5,}";
+
+            Logging.Print(Regex.Replace(packet.PayloadToString(), pattern, " ").Trim(), Logging.LogType.Debug);
         }
     }
 }
