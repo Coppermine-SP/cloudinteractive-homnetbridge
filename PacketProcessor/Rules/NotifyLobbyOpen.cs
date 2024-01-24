d79db052511fd4f5308f4ea34d90e8c802ae4d29
Coppermine-SP <son5121502@gmail.com>
2024-01-25 03:27:15 +0900

    feat: NotifyLobbyOpen Rule

---
+++ PacketProcessor/Rules/NotifyLobbyOpen.cs
@@ -0,0 +1,21 @@
+﻿using HomNetBridge.Services;
+using PacketDotNet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomNetBridge.PacketProcessor
+{
+    public static partial class Rules
+    {
+        [Protocol(ProtocolType.Udp)]
+        [Contains("CallType=TYPE_LOBBY,MsgType=reqOpenDoor")]
+        public static void NotifyLobbyOpen(IPPacket packet)
+        {
+            Logging.Print($"Lobby reqOpenDoor event detected in HBM REPORT!");
+            NotifyService.Notify("공동현관문", $"공동현관문 출입 요청을 승인했습니다.");
+        }
+    }
+}
