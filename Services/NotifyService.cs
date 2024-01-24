47666d83205e2425c2a9b952d7081bb15b987836
Coppermine-SP <son5121502@gmail.com>
2024-01-24 15:45:07 +0900

    feat: Home assistant notify service

---
+++ Services/NotifyService.cs
@@ -0,0 +1,31 @@
+﻿using HADotNet.Core;
+using HADotNet.Core.Clients;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomNetBridge.Services
+{
+    public static class NotifyService
+    {
+        public static void Init(string endpoint, string key)
+        {
+            Logging.Print($"NotifyService Init... (endpoint={endpoint})");
+            ClientFactory.Initialize(endpoint, key);
+        }
+
+        public static void Notify(string title, string message)
+        {
+            var serviceClient = ClientFactory.GetClient<ServiceClient>();
+
+            Logging.Print($"NotifyService.Notify : {title} / {message}", Logging.LogType.Debug);
+            serviceClient.CallService("notify.notify", new
+            {
+                title = title,
+                message = message
+            });
+        }
+    }
+}
