a4059fffb729902bbbcfa12481a80e7ef36ec41b
Coppermine-SP <son5121502@gmail.com>
2024-01-25 03:27:28 +0900

    feat: NotifyLobbyRing Rule

---
+++ PacketProcessor/Rules/NotifyLobbyRing.cs
@@ -0,0 +1,17 @@
+﻿using HomNetBridge.Services;
+using PacketDotNet;
+
+
+namespace HomNetBridge.PacketProcessor
+{
+    public static partial class Rules
+    {
+        [Protocol(ProtocolType.Udp)]
+        [Contains("CallType=TYPE_LOBBY,MsgType=evtRing")]
+        public static void NotifyLobbyRing(IPPacket packet)
+        {
+            Logging.Print($"Lobby evtRing event detected in HBM REPORT!");
+            NotifyService.Notify("공동현관문", $"공동현관문 출입 요청이 있습니다.");
+        }
+    }
+}
