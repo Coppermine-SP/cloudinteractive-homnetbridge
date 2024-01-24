304c79f36ed9d12f1738ab3c2b22da17c2de7914
Coppermine-SP <son5121502@gmail.com>
2024-01-25 03:28:32 +0900

    feat: GetUrlParameter

---
+++ PacketProcessor/Util/GetUrlParameter.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json.Linq;
+using PacketDotNet;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace HomNetBridge.PacketProcessor
+{
+    public static partial class Util
+    {
+        public static string GetUrlParameter(string key, string value)
+        {
+            NameValueCollection parameters = HttpUtility.ParseQueryString(value);
+            return parameters[key];
+        }
+    }
+}
