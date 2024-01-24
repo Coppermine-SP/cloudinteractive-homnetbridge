fde4e226743d61981ddf72259c0a2d186b87a4bf
Coppermine-SP <son5121502@gmail.com>
2024-01-25 03:27:06 +0900

    feat: ElevatorFloorUpdate Rule

---
+++ PacketProcessor/Rules/ElevatorFloorUpdate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HomNetBridge.Services;
+using PacketDotNet;
+
+namespace HomNetBridge.PacketProcessor
+{
+    public static partial class Rules
+    {
+        [Protocol(ProtocolType.Tcp)]
+        [StartsWith("SLB&8&lbs&20&cmd=2")]
+        [UrlParameter("floor")]
+        public static void ElevatorFloorUpdate(IPPacket packet)
+        {
+            string content = packet.PayloadToString();
+            string floor = Util.GetUrlParameter("floor", content);
+            int direction = Convert.ToInt32(Util.GetUrlParameter("direction", content));
+            ElevatorService.UpdateFloor(floor, direction);
+        }
+    }
+}
