411964a07ae1770eca88c5e6838fb97b4c39519b
Coppermine-SP <son5121502@gmail.com>
2024-01-24 15:45:18 +0900

    feat: NotifyVehicleEntry rule

---
+++ PacketProcessor/Rules/NotifyVehicleEntry.cs
@@ -0,0 +1,25 @@
+﻿using PacketDotNet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HomNetBridge.Services;
+
+namespace HomNetBridge.PacketProcessor
+{
+    public static partial class Rules
+    {
+        [Protocol(ProtocolType.Tcp)]
+        [StartsWith("SLB&5&lbs&17")]
+        [UrlParameter("info")]
+        public static void NotifyVehicleEntry(IPPacket packet)
+        {
+            string content = packet.PayloadToString(true);
+            string licenseNo = Util.GetUrlParameter("info", content);
+            
+            Logging.Print($"VehicleEntry: {licenseNo}");
+            NotifyService.Notify("차량 입차", $"{licenseNo} 차량이 입차했습니다.");
+        }
+    }
+}
