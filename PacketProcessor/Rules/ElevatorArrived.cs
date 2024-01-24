4fd637c306f3dbf20d909f30450b91ea417f8535
Coppermine-SP <son5121502@gmail.com>
2024-01-25 03:26:39 +0900

    feat: ElevatorArrived Rule

---
+++ PacketProcessor/Rules/ElevatorArrived.cs
@@ -0,0 +1,20 @@
+﻿using PacketDotNet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomNetBridge.PacketProcessor
+{
+    public static partial class Rules
+    {
+        [Protocol(ProtocolType.Tcp)]
+        [StartsWith("SLB&5&lbs&20")]
+        [UrlParameter("arrival")]
+        public static void ElevatorArrived(IPPacket packet)
+        {
+            Services.ElevatorService.ElevatorArrived();
+        }
+    }
+}
