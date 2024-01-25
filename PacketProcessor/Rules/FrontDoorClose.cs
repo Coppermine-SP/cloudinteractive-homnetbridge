b59aff1e148a3c52b8ce79c9c79a11032037f0ca
Coppermine-SP <son5121502@gmail.com>
2024-01-25 19:10:33 +0900

    feat: FrontDoorClose, Open rules

---
+++ PacketProcessor/Rules/FrontDoorClose.cs
@@ -0,0 +1,21 @@
+﻿using HomNetBridge.Services;
+using PacketDotNet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomNetBridge.PacketProcessor
+{
+    public static partial class Rules
+    {
+        [Protocol(ProtocolType.Udp)]
+        [Contains("F3 09 82 02 01 01 89 F4")]
+        public static void FrontDoorClose(IPPacket packet)
+        {
+            Logging.Print("Security sensor close event detected in HBM REPORT!");
+            HAService.ChangeDoorBinarySensorState(false);
+        }
+    }
+}
---
+++ PacketProcessor/Rules/FrontDoorOpen.cs
@@ -0,0 +1,21 @@
+﻿using HomNetBridge.Services;
+using PacketDotNet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomNetBridge.PacketProcessor
+{
+    public static partial class Rules
+    {
+        [Protocol(ProtocolType.Udp)]
+        [Contains("F3 09 82 02 00 01 88 F4")]
+        public static void FrontDoorOpen(IPPacket packet)
+        {
+            Logging.Print("Security sensor open event detected in HBM REPORT!");
+            HAService.ChangeDoorBinarySensorState(true);
+        }
+    }
+}
